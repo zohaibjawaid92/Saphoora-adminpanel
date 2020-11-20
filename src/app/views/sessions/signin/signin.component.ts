f514be5cecf2b4ab0f1edb10739d87c349df391e
zohaibjawaid92 <zohaibjawaid92@gmail.com>
2020-11-20 15:39:54 +0500

    first push

---
+++ src/app/views/sessions/signin/signin.component.ts
@@ -0,0 +1,36 @@
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { MatProgressBar, MatButton } from '@angular/material';
+import { Validators, FormGroup, FormControl } from '@angular/forms';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-signin',
+  templateUrl: './signin.component.html',
+  styleUrls: ['./signin.component.css']
+})
+export class SigninComponent implements OnInit {
+  @ViewChild(MatProgressBar) progressBar: MatProgressBar;
+  @ViewChild(MatButton) submitButton: MatButton;
+
+  signinForm: FormGroup;
+
+  constructor(public router : Router) { }
+
+  ngOnInit() {
+    this.signinForm = new FormGroup({
+      username: new FormControl('', Validators.required),
+      password: new FormControl('', Validators.required),
+      rememberMe: new FormControl(false)
+    })
+  }
+
+  signin() {
+    const signinData = this.signinForm.value
+    console.log(signinData);
+    this.router.navigate(['/'])
+    // this.submitButton.disabled = true;
+    // this.progressBar.mode = 'indeterminate';
+
+  }
+
+}
